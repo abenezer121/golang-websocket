e1e2964c8c316723454c9c5e997d99ab51bbe155
abenezer121 <webgoat12@gmail.com>
2025-05-12 12:44:23 +0300

    [mod]- adding client id in the request

--- server/models/request.go
+++ server/models/request.go
@@ -4,6 +4,7 @@ import "time"
 
 type Command struct {
 	//Id          string          `json:"id,omitempty"`
+	ClientId    *string    `json:"client_id,omitempty"`
 	CommandType *string    `json:"command_type,omitempty"`
 	DriverId    *string    `json:"driver_id,omitempty"`
 	MinLat      *float64   `json:"min_lat,omitempty"`
@@ -11,10 +12,10 @@ type Command struct {
 	MaxLat      *float64   `json:"max_lat,omitempty"`
 	MaxLng      *float64   `json:"max_lng,omitempty"`
 	Page        *int       `json:"page,omitempty"`
-	Lat         *float64   `json:"lat"`
-	Lng         *float64   `json:"lng"`
-	From        *string    `json:"from"`
-	Session     *string    `json:"session"`
-	At          *time.Time `json:"at"`
-	Active      *bool      `json:"active"`
+	Lat         *float64   `json:"lat,omitempty"`
+	Lng         *float64   `json:"lng,omitempty"`
+	From        *string    `json:"from,omitempty"`
+	Session     *string    `json:"session,omitempty"`
+	At          *time.Time `json:"at,omitempty"`
+	Active      *bool      `json:"active,omitempty"`
 }
